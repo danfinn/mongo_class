c9af17ef81e7575ee87418cc14aeafe3b4087a8c
danfinn <dan_j_finn@yahoo.com>
2015-01-12 18:29:19 +0200

    bottle templating

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,9 @@
-from bottle import route, run, template
+from bottle import route, run, template, debug
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/')
+def home_page():
+    mythings = ['bike','skis','flyrod']
+    return template('hello_world', username='Dan', things=mythings)
 
+debug(True)
 run(host='localhost', port=8080)
